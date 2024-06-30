6fe7e0929ecaaa316e9d740c02f27389923cc000
Godand <aezeigwe2003@gmail.com>
2024-06-30 13:37:28 +0200

    set up stage1 files

---
+++ Stage1/SimpleShoppingApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SimpleShoppingApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Stage1/SimpleShoppingApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace SimpleShoppingApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
